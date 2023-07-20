5ad34708045510b22e014b857da0b08862b0ac21
Aayush Sahay <asstore3@gmail.com>
2023-07-20 19:26:50 +0530

    Completed one card with errors and not responsive

--- lib/UI/SectionsOfHomePage/RecentWorks/recent_Works.dart
+++ lib/UI/SectionsOfHomePage/RecentWorks/recent_Works.dart
@@ -12,8 +12,8 @@ class _RecentWorksState extends State<RecentWorks> {
   Widget build(BuildContext context) {
     return Container(
       constraints: BoxConstraints.expand(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height,
       ),
       decoration: BoxDecoration(
         color: const Color(0xFFF7E8FF).withOpacity(0.3),
@@ -30,148 +30,232 @@ class _RecentWorksState extends State<RecentWorks> {
             top: -70,
             //todo This needs to be set such that the top child is half way through the container
             left: 0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / 5,
-                  width: MediaQuery.of(context).size.width,
-                  child: Card(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Image(
-                          image: AssetImage('assets/graphic.png'),
+            child: Expanded(
+              child: SizedBox(
+                //todo fix the error of unbounded width here. replace the temporary fix
+                // height: MediaQuery.sizeOf(context).height / 2,
+                width: MediaQuery.sizeOf(context).width,
+                child: Column(
+                  // mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.sizeOf(context).height / 5,
+                      width: MediaQuery.sizeOf(context).width,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        //todo change the image to the one in the source of website
-                        const VerticalDivider(
-                          color: Colors.black,
-                          thickness: 0.5,
-                          width: 1,
-                          indent: 10,
-                          endIndent: 10,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        elevation: 0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Text(
-                              'Starting New Project?',
-                              style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width / 60,
+                            const Flexible(
+                              child: Image(
+                                image: AssetImage('assets/graphic.png'),
                               ),
                             ),
-                            const Text('Get an estimate for the new project'),
+                            //todo change the image to the one in the source of website
+                            const Flexible(
+                              child: VerticalDivider(
+                                color: Colors.black,
+                                thickness: 0.5,
+                                width: 1,
+                                indent: 10,
+                                endIndent: 10,
+                              ),
+                            ),
+                            Flexible(
+                              flex: 2,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Starting New Project?',
+                                    style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.sizeOf(context).width / 60,
+                                    ),
+                                  ),
+                                  const Text(
+                                      'Get an estimate for the new project'),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.sizeOf(context).width / 2,
+                            ),
+                            Flexible(
+                              child: TextButton.icon(
+                                style: const ButtonStyle(
+                                  padding: MaterialStatePropertyAll<
+                                      EdgeInsetsGeometry>(
+                                    EdgeInsets.all(22),
+                                  ),
+                                  side: MaterialStatePropertyAll<BorderSide>(
+                                    BorderSide(
+                                      width: 0.1,
+                                    ),
+                                  ),
+                                  shape: MaterialStatePropertyAll<StadiumBorder>(
+                                    StadiumBorder(),
+                                  ),
+                                ),
+                                onPressed: () => {},
+                                icon: Image.asset('assets/hand.png'),
+                                label: const Text('Hire Me!'),
+                                // icon: Image.asset('assets/hand.png'),
+                                // label: const Text('Download CV'),
+                              ),
+                            ),
                           ],
                         ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width / 2,
-                        ),
-                        TextButton.icon(
-                          style: const ButtonStyle(
-                            padding:
-                                MaterialStatePropertyAll<EdgeInsetsGeometry>(
-                              EdgeInsets.all(22),
-                            ),
-                            side: MaterialStatePropertyAll<BorderSide>(
-                              BorderSide(
-                                width: 0.1,
-                              ),
-                            ),
-                            shape: MaterialStatePropertyAll<StadiumBorder>(
-                              StadiumBorder(),
-                            ),
-                          ),
-                          onPressed: () => {},
-                          icon: Image.asset('assets/hand.png'),
-                          label: const Text('Hire Me!'),
-                          // icon: Image.asset('assets/hand.png'),
-                          // label: const Text('Download CV'),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    // SizedBox(width: MediaQuery.of(context).size.width/20,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: <Widget>[
-                        Container(
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              left: BorderSide(
-                                width: 7,
-                                color: Colors.red,
+                        // SizedBox(width: MediaQuery.sizeOf(context).width/20,),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    left: BorderSide(
+                                      width: 7,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                    left: MediaQuery.sizeOf(context).width / 20,
+                                  ),
+                                  child: Text(
+                                    'My Strong Arenas',
+                                    style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.sizeOf(context).width / 60,
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                              left: MediaQuery.of(context).size.width / 20,
-                            ),
-                            child: Text(
-                              'My Strong Arenas',
-                              style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width / 60,
+                              Container(
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    left: BorderSide(
+                                      width: 7,
+                                    ),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                    left: MediaQuery.sizeOf(context).width / 20,
+                                  ),
+                                  child: Text(
+                                    'Recent Works',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w900,
+                                      fontSize:
+                                          MediaQuery.sizeOf(context).width / 20,
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
+                            ],
                           ),
                         ),
-                        Container(
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              left: BorderSide(
-                                width: 7,
+                      ],
+                    ),
+                    Row(
+                      // mainAxisSize: MainAxisSize.min,
+                      //todo create a list.generate list to create the lists.
+                      // mainAxisSize: ,
+                      children: [
+                        // Expanded(
+                        //   child: ListTile(
+                        //     leading: SizedBox(
+                        //       height: MediaQuery.sizeOf(context).height / 5,
+                        //       width: MediaQuery.sizeOf(context).width / 5,
+                        //       child: Image.asset('assets/work1.png'),
+                        //     ),
+                        //     title: const Text('Graphic Design'),
+                        //     subtitle: const Text(
+                        //       'New & Fresh Looking Portfolio indeed at the end',
+                        //     ),
+                        //   ), //todo change the listTile to custom row implementation of desired design.
+                        // ),
+                        // Expanded(
+                        //   child: ListTile(
+                        //     leading: SizedBox(
+                        //         height: MediaQuery.sizeOf(context).height / 5,
+                        //         width: MediaQuery.sizeOf(context).width / 5,
+                        //         child: Image.asset('assets/work_2.png')),
+                        //     title: const Text('Graphic Design'),
+                        //     subtitle: const Text(
+                        //         'New & Fresh Looking Portfolio indeed at the end'),
+                        //   ),
+                        // ),
+                        Expanded(
+                          child: FractionallySizedBox(
+                            widthFactor: 0.4,
+                            child: Card(
+                              semanticContainer: false,
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                              left: MediaQuery.of(context).size.width / 20,
-                            ),
-                            child: Text(
-                              'Recent Works',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                fontSize:
-                                    MediaQuery.of(context).size.width / 20,
+                              child: SizedBox(
+                                height: 160,
+                                width: 100,
+                                child: Row(
+                                  children: [
+                                    Card(
+                                      margin: EdgeInsets.zero,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      elevation: 0,
+                                      child: const Image(
+                                        image: AssetImage('assets/work1.png'),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width:
+                                          MediaQuery.sizeOf(context).width / 19,
+                                    ),
+                                    const Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Expanded(child: Text('GRAPHIC DESIGN')),
+                                        Expanded(
+                                          child: Center(
+                                            child: Text(
+                                              'New  Fresh Looking Portfolio indeed at the end',
+                                              softWrap: true,
+                                              maxLines: 2,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                        ),
+                                        Text('View Details'),
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ],
-                    ),
+                    )
                   ],
                 ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  //todo create a list.generate list to create the lists.
-                  // mainAxisSize: ,
-                  children: [
-                    Expanded(
-                      child: ListTile(
-                        leading: Image.asset('assets/work1.png'),
-                        title: const Text('Graphic Design'),
-                        subtitle: const Text(
-                          'New & Fresh Looking Portfolio indeed at the end',
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: ListTile(
-                        leading: Image.asset('assets/work_2.png'),
-                        title: Text('Graphic Design'),
-                        subtitle: Text(
-                            'New & Fresh Looking Portfolio indeed at the end'),
-                      ),
-                    ),
-                  ],
-                )
-              ],
+              ),
             ),
           ),
         ],
