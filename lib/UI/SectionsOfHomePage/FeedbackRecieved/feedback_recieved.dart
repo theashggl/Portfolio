9ee6437c5a2f37f7016435978fd97c44f66f8688
Aayush Sahay <asstore3@gmail.com>
2023-07-22 01:58:22 +0530

    recent work ui ready with random colour combination.

--- lib/UI/SectionsOfHomePage/FeedbackRecieved/feedback_recieved.dart
+++ lib/UI/SectionsOfHomePage/FeedbackRecieved/feedback_recieved.dart
@@ -24,189 +24,244 @@ class _FeedbackRecievedState extends State<FeedbackRecieved> {
             Row(
               children: <Widget>[
                 // SizedBox(width: MediaQuery.sizeOf(context).width/20,),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            left: BorderSide(
-                              width: 7,
-                              color: Colors.red,
-                            ),
-                          ),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                            left: MediaQuery.sizeOf(context).width / 20,
-                          ),
-                          child: Text(
-                            'My Strong Arenas',
-                            style: TextStyle(
-                              fontSize: MediaQuery.sizeOf(context).width / 60,
-                            ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          left: BorderSide(
+                            width: 7,
+                            color: Colors.red,
                           ),
                         ),
                       ),
-                      Container(
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            left: BorderSide(
-                              width: 7,
-                            ),
-                          ),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: MediaQuery.sizeOf(context).width / 20,
                         ),
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                            left: MediaQuery.sizeOf(context).width / 20,
-                          ),
-                          child: Text(
-                            'Recent Works',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w900,
-                              fontSize: MediaQuery.sizeOf(context).width / 20,
-                            ),
+                        child: Text(
+                          'My Strong Arenas',
+                          style: TextStyle(
+                            fontSize: MediaQuery.sizeOf(context).width / 60,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          left: BorderSide(
+                            width: 7,
+                          ),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: MediaQuery.sizeOf(context).width / 20,
+                        ),
+                        child: Text(
+                          'Recent Works',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: MediaQuery.sizeOf(context).width / 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
+            const SizedBox(
+              height: 60,
+            ), //todo remove this sizedbox and replace the code to do its job.
             Wrap(
               alignment: WrapAlignment.spaceEvenly,
-              children: [
+              children: [//todo convert children to auto generated list of them
                 SizedBox(
-                  width: MediaQuery.sizeOf(context).width/7,
+                  width: MediaQuery.sizeOf(context).width / 4,
                   child: Card(
                     elevation: 0,
                     color: Colors.blueAccent,
-                    child: Expanded(
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: MediaQuery.sizeOf(context).width / 35,
-                            backgroundColor: Colors.white,
-                            child: CircleAvatar(
-                              radius: MediaQuery.sizeOf(context).width / 40,
-                              backgroundImage: const AssetImage(
-                                'assets/people.png',
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(),
+                            Positioned(
+                              top: -35,
+                              left: 115,
+                              child: CircleAvatar(
+                                radius: MediaQuery.sizeOf(context).width / 35,
+                                backgroundColor: Colors.white,
+                                child: CircleAvatar(
+                                  radius: MediaQuery.sizeOf(context).width / 40,
+                                  backgroundImage: const AssetImage(
+                                    'assets/people.png',
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                          const Text(
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                            const Positioned(
+                              left: 120,
+                              top: 100,
+                              child: Icon(
+                                Icons.format_quote_outlined,
+                                color: Colors.yellow,
+                                size: 75,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                          ],
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(
+                              left: 40.0, right: 40.0, bottom: 20.0, top: 40),
+                          child: Text(
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
                             // softWrap: true,
+                            textAlign: TextAlign.center,
                             maxLines: 8,
                             overflow: TextOverflow.ellipsis,
                           ),
-                          const Text(
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 18.0),
+                          child: Text(
                             'Ronald Thompson',
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
                 SizedBox(
-                  width: MediaQuery.sizeOf(context).width/7,
+                  width: MediaQuery.sizeOf(context).width / 4,
                   child: Card(
                     elevation: 0,
                     color: Colors.blueAccent,
-                    child: Expanded(
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: MediaQuery.sizeOf(context).width / 35,
-                            backgroundColor: Colors.white,
-                            child: CircleAvatar(
-                              radius: MediaQuery.sizeOf(context).width / 40,
-                              backgroundImage: const AssetImage(
-                                'assets/people.png',
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(),
+                            Positioned(
+                              top: -35,
+                              left: 115,
+                              child: CircleAvatar(
+                                radius: MediaQuery.sizeOf(context).width / 35,
+                                backgroundColor: Colors.white,
+                                child: CircleAvatar(
+                                  radius: MediaQuery.sizeOf(context).width / 40,
+                                  backgroundImage: const AssetImage(
+                                    'assets/people.png',
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                          const Text(
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                            const Positioned(
+                              left: 120,
+                              top: 100,
+                              child: Icon(
+                                Icons.format_quote_outlined,
+                                color: Colors.yellow,
+                                size: 75,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                          ],
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(
+                              left: 40.0, right: 40.0, bottom: 20.0, top: 40),
+                          child: Text(
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
                             // softWrap: true,
+                            textAlign: TextAlign.center,
                             maxLines: 8,
                             overflow: TextOverflow.ellipsis,
                           ),
-                          const Text(
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 18.0),
+                          child: Text(
                             'Ronald Thompson',
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
                 SizedBox(
-                  width: MediaQuery.sizeOf(context).width/7,
+                  width: MediaQuery.sizeOf(context).width / 4,
                   child: Card(
                     elevation: 0,
                     color: Colors.blueAccent,
-                    child: Expanded(
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: MediaQuery.sizeOf(context).width / 35,
-                            backgroundColor: Colors.white,
-                            child: CircleAvatar(
-                              radius: MediaQuery.sizeOf(context).width / 40,
-                              backgroundImage: const AssetImage(
-                                'assets/people.png',
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(),
+                            Positioned(
+                              top: -35,
+                              left: 115,
+                              child: CircleAvatar(
+                                radius: MediaQuery.sizeOf(context).width / 35,
+                                backgroundColor: Colors.white,
+                                child: CircleAvatar(
+                                  radius: MediaQuery.sizeOf(context).width / 40,
+                                  backgroundImage: const AssetImage(
+                                    'assets/people.png',
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                          const Text(
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                            const Positioned(
+                              left: 120,
+                              top: 100,
+                              child: Icon(
+                                Icons.format_quote_outlined,
+                                color: Colors.yellow,
+                                size: 75,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                          ],
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(
+                              left: 40.0, right: 40.0, bottom: 20.0, top: 40),
+                          child: Text(
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
                             // softWrap: true,
+                            textAlign: TextAlign.center,
                             maxLines: 8,
                             overflow: TextOverflow.ellipsis,
                           ),
-                          const Text(
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 18.0),
+                          child: Text(
                             'Ronald Thompson',
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.sizeOf(context).width/7,
-                  child: Card(
-                    elevation: 0,
-                    color: Colors.blueAccent,
-                    child: Expanded(
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: MediaQuery.sizeOf(context).width / 35,
-                            backgroundColor: Colors.white,
-                            child: CircleAvatar(
-                              radius: MediaQuery.sizeOf(context).width / 40,
-                              backgroundImage: const AssetImage(
-                                'assets/people.png',
-                              ),
-                            ),
-                          ),
-                          const Text(
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                            // softWrap: true,
-                            maxLines: 8,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const Text(
-                            'Ronald Thompson',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
